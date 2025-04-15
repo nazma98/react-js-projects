4ae5fd0310cbd450645e22923aa96add60e53bc0
nazma98 <nazmasarker1311@gmail.com>
2025-04-15 16:31:05 +0600

    Add Navigation component and brand logo image

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import Navigation from './components/Navigation';
+
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <>Hello World!</>;
-}
+const App = () => {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+};
 
 export default App;
