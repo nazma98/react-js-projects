82cba8764807bc0d7b0bdad7b7147677e43b4f45
nazma98 <nazmasarker1311@gmail.com>
2025-06-10 13:01:10 +0600

    Add Favorites page

---
+++ react-movie/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites() {
+  return (
+    <div className='favrites-empty'>
+      <h2>No favorites yet</h2>
+      <p>Start adding movies to your favorites and they will appear here!</p>
+    </div>
+  );
+}
+
+export default Favorites;
