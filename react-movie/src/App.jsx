d4f4c2c4dfa99e6014387cf88aeeb6e45593de1c
nazma98 <nazmasarker1311@gmail.com>
2025-06-10 12:44:29 +0600

    Add pages for using in component
    - Add Home page
    - Add favorite page

--- react-movie/src/App.jsx
+++ react-movie/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
 import MovieCard from './components/MovieCard';
+import Home from './pages/Home';
 
 function App() {
+  const movieNumber = 1;
 
   return (
     <>
- <MovieCard movie={{title: "Huckleberry Finn", release_date: "2024"}}/>
+      <Home />
     </>
-  )
-};
+  );
+}
 
-export default App
+export default App;
